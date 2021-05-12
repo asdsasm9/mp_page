94f4dc89488be84acb000a29499d27026dd9a15d
asdsasm9 <adamkollar1@gmail.com>
2021-05-12 12:50:05 +0200

    fotky zmensene

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  portfolio_part(1);
   $(".nav-toggler").each(function (_, navToggler) {
     var target = $(navToggler).data("target");
     $(navToggler).on("click", function () {
@@ -8,14 +7,49 @@ $(document).ready(function () {
       });
     });
   });
+  
+  var urlParams;
+  (window.onpopstate = function () {
+      var match,
+          pl     = /\+/g,  // Regex for replacing addition symbol with a space
+          search = /([^&=]+)=?([^&]*)/g,
+          decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+          query  = window.location.search.substring(1);
+
+      urlParams = {};
+      while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+  })();
+
+  var typ = urlParams["site"];
+  portfolio_part(typ);
+
+  
 });
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 400) {
+    $('.move_up_btn').fadeIn();
+  } else {
+    $('.move_up_btn').fadeOut();
+  }
+});
+
+function move_up(){
+  $('html, body').animate({
+    scrollTop: 0
+  }, 800);
+}
+
+
 function portfolio_part(i) {
   var x = document.getElementById("svadby");
   var y = document.getElementById("portrety");
-  var z = document.getElementById("produktovky");
+  var z = document.getElementById("produkty");
   var xx = document.getElementById("svadby_btn");
   var yy = document.getElementById("portrety_btn");
-  var zz = document.getElementById("produktovky_btn");
+  var zz = document.getElementById("produkty_btn");
   x.style.display = "none";
   y.style.display = "none";
   z.style.display = "none";
@@ -23,16 +57,17 @@ function portfolio_part(i) {
   yy.style.textDecoration = "none";
   zz.style.textDecoration = "none";
 
-  if (i === 1) {
-    xx.style.textDecoration = "underline overline";
-    x.style.display = "block";
-  }
-  if (i === 2) {
+  if (i === "portrety") {
     yy.style.textDecoration = "underline overline";
     y.style.display = "block";
   }
-  if (i === 3) {
+  if (i === "produkty") {
     zz.style.textDecoration = "underline overline";
     z.style.display = "block";
   }
+  if (i === "svadby" || i === undefined) {
+    xx.style.textDecoration = "underline overline";
+    x.style.display = "block";
+  }
+  
 }

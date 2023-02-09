107116efa8087bc32db4f55c414160d5e28b75b4
xkollar9 <xkollar9@fi.muni.cz>
2023-02-09 19:05:57 +0100

    aboutView works on mobile

--- src/components/about/AboutView.tsx
+++ src/components/about/AboutView.tsx
@@ -8,16 +8,18 @@ const AboutView = (props: {
   textOnRight: boolean;
 }) => {
   const myImg = (
-    <div className="w-1/2 p-4">
+    <div className="w-full md:w-1/2 p-4 items-center flex">
       <img src={props.imgSrc} alt="Image" className="textAboutImg" />
     </div>
   );
   return (
-    <div className="flex mx-10 mb-10">
+    <div className="md:flex mx-10 mb-10">
       {props.textOnRight ? myImg : ""}
-      <div className="w-1/2 px-10 p-10">
-        <p className="textAboutTitle pt-3">{props.title}</p>
-        <p className="textAboutDescription pt-10">{props.description}</p>
+      <div className="w-full p-4 md:1/2 md:px-10 md:p-10">
+        <p className="textAboutTitle md:pt-3">{props.title}</p>
+        <p className="textAboutDescription pt-4 md:pt-10">
+          {props.description}
+        </p>
       </div>
       {!props.textOnRight ? myImg : ""}
     </div>

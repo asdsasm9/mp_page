a240fdbb8a3fa5ee1ca8dfbf3ea242be34034846
xkollar9 <xkollar9@fi.muni.cz>
2023-03-16 15:05:04 +0100

    deleted BASE_URL

--- src/data/Translations.ts
+++ src/data/Translations.ts
@@ -1,4 +1,3 @@
 export const KnowMore = "ZOBRAZIT VÍC";
 export const InstagramUrl = "https://www.instagram.com/mullny_photos/?hl=cs";
 export const MailUrl = "mailto: mullnyphotos@gmail.com";
-export const BASE_URL = "/mp_page";
